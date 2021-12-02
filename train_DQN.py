789498471a134a028b826f5156c91ae41920cf79
Kycipapy Donahue <kycipapydonahue@Dehaos-Mac-mini.fios-router.home>
2021-12-02 18:14:43 -0500

    add config files

--- train_DQN.py
+++ train_DQN.py
@@ -1,23 +1,35 @@
 import gym
-import torch
 from Agent import Agent
 from vars import *
 import matplotlib.pyplot as plt
 import copy
 from statsmodels.tsa.api import SimpleExpSmoothing
+import argparse
+import json
+
+parser = argparse.ArgumentParser(description='Please provide the config file.')
+parser.add_argument('-config', type=str, help='the path to the config file')
+args = parser.parse_args()
+with open(args.config) as f:
+    config = json.load(f)
 
 env = gym.make('CartPole-v0')
 input_dim = env.observation_space.shape[0]
 output_dim = env.action_space.n
-agent = Agent(seed=1423, layer_sizes=[input_dim, HIDDEN_LAYER_SIZE, output_dim], lr=LR, sync_freq=SYNC_FREQ, exp_replay_size=EXP_REPLAY_SIZE, gamma=GAMMA)
+agent = Agent(seed=1423,  
+    layer_sizes=[input_dim, config['hidden_layer_size'], output_dim], 
+    lr=config['lr'], 
+    sync_freq=config['sync_freq'], 
+    exp_replay_size=config['exp_replay_size'],
+    gamma=config['gamma'])
 agent.initialize(env)
 
 durations = []
-index = EXP_REPLAY_SIZE / 2
-epsilon = EPS_START
+index = config['exp_replay_size'] / 2
+epsilon = config['eps_start']
 best_agent = agent
 
-for i in range(N_EPISODES):
+for i in range(config['n_episodes']):
     obs, done, duration = env.reset(), False, 0
     while (not done):
         duration += 1 
@@ -28,20 +40,21 @@ for i in range(N_EPISODES):
         obs = obs_next
         index += 1
         
-        if(index > EXP_REPLAY_SIZE / 2):
+        if(index > config['exp_replay_size'] / 2):
             index = 0
             for _ in range(4):
-                q_loss, reg_loss = agent.compute_loss(batch_size=BATCH_SIZE, reg={'K': 3, 'gamma': 2, 'sigma': 0.5, 'lambda': 0.01})
-                loss = q_loss + reg_loss
+                q_loss, reg_loss = agent.compute_loss(batch_size=config['batch_size'], reg=config['reg'])
+                loss = q_loss + reg_loss if reg_loss is not None else q_loss
                 agent.optimizer.zero_grad()
                 loss.backward(retain_graph=True)
                 agent.optimizer.step()
 
     if i % 100 == 0:         
-        print(f'episode {i}: {q_loss.item()}, {reg_loss.item()}.')
+        print(f'episode {i}, q_loss: {q_loss.item()}, reg_loss: {reg_loss.item() if reg_loss is not None else None}')
+        pass
 
-    if epsilon > EPS_END:
-        epsilon -= (1 / EPS_DECAY)
+    if epsilon > config['eps_end']:
+        epsilon -= (1 / config['eps_decay'])
 
     durations.append(duration)
     if duration >= max(durations):
@@ -50,16 +63,20 @@ for i in range(N_EPISODES):
 fit1 = SimpleExpSmoothing(durations, initialization_method="heuristic").fit(smoothing_level=0.05, optimized=False)
 plt.plot(durations)
 plt.plot(fit1.fittedvalues)
-plt.savefig('hello.jpg')
+plt.xlabel("Episodes")
+plt.ylabel("Duration")
+plt.title("Performance of the Agent as Training.")
+plt.savefig(config['figpath'])
 plt.close()
 
 env = gym.make('CartPole-v0')
-for i in range(10):
-    obs, done, r = env.reset(), False, 0
+r = 0
+for i in range(1000):
+    obs, done = env.reset(), False
     while (not done):
         A =  best_agent.get_action(obs, env.action_space.n, epsilon = 0)
         obs, reward, done, _ = env.step(A.item())
         r += 1
-    print(f'episode {i}, duration {r}.')
 
-agent.save('models/DQN.pth')
+print(f"Try 1000 episodes. Average reward is {r / 1000}.")
+agent.save(config['savepath'])
--- train_attacker.py
+++ train_attacker.py
@@ -7,19 +7,36 @@ import matplotlib.pyplot as plt
 import copy
 from statsmodels.tsa.api import SimpleExpSmoothing
 
+import argparse
+import json
+parser = argparse.ArgumentParser(description='Please provide the config file.')
+parser.add_argument('-attack_config', type=str, help='the path to the attacker config file')
+parser.add_argument('-agent_config', type=str, help='the path to the agent config file')
+args = parser.parse_args()
+with open(args.attack_config) as f:
+    attack_cfg = json.load(f)
+with open(args.agent_config) as f:
+    agent_cfg = json.load(f)
+    
+
 env = gym.make('CartPole-v0')
 input_dim = env.observation_space.shape[0]
 output_dim = env.action_space.n
-agent = Agent(seed=1423, layer_sizes=[input_dim, HIDDEN_LAYER_SIZE, output_dim], lr=999, sync_freq=SYNC_FREQ, exp_replay_size=EXP_REPLAY_SIZE, gamma=GAMMA)
-agent.q_net.load_state_dict(torch.load('models/DQN.pth'))
-attacker = Attacker(hidden_layer_size=HIDDEN_LAYER_SIZE, norm_bound=NORM_BOUND, num_layers=NUM_LAYERS)
-optimizer = torch.optim.Adam(attacker.parameters(), lr=LR)
+agent = Agent(seed=1423,  
+    layer_sizes=[input_dim, agent_cfg['hidden_layer_size'], output_dim], 
+    lr=agent_cfg['lr'], 
+    sync_freq=agent_cfg['sync_freq'], 
+    exp_replay_size=agent_cfg['exp_replay_size'],
+    gamma=agent_cfg['gamma'])
+agent.q_net.load_state_dict(torch.load(agent_cfg['savepath']))
+attacker = Attacker(hidden_layer_size=agent_cfg['hidden_layer_size'], norm_bound=attack_cfg['norm_bound'], num_layers=attack_cfg['num_layers'])
+optimizer = torch.optim.Adam(attacker.parameters(), lr=attack_cfg['lr'])
 
 durations = []
-index = EXP_REPLAY_SIZE / 2
+index = agent_cfg['exp_replay_size'] / 2
 best_attacker = attacker
 
-for i in range(N_EPISODES):
+for i in range(attack_cfg['n_episodes']):
     obs, done, duration = env.reset(), False, 0
     while (not done):
         duration += 1 
@@ -48,12 +65,13 @@ plt.savefig('hello.jpg')
 plt.close()
 
 env = gym.make('CartPole-v0')
-for i in range(10):
-    obs, done, r = env.reset(), False, 0
+r = 0
+for i in range(1000):
+    obs, done = env.reset(), False
     while (not done):
         A =  agent.get_action(obs, env.action_space.n, epsilon=0, attacker=best_attacker)
         obs, reward, done, _ = env.step(A.item())
         r += 1
-    print(f'episode {i}, duration {r}.')
+print(f'Try 1000 episodes. Average reward is {r / 1000}.')
 
 torch.save(best_attacker.state_dict(), 'models/attacker.pth')
