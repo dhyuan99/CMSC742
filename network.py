21db24b0895c6784d364b3b9bcf5cb762daf4264
Kycipapy Donahue <kycipapydonahue@Dehaos-Mac-mini.fios-router.home>
2021-12-01 22:31:51 -0500

    finish DQN

--- network.py
+++ network.py
@@ -1,99 +1,83 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
+from collections import deque
+import random
+import copy
 
-from vars import BATCH_SIZE, GAMMA
-from utils import Transition
-
-class DQN(nn.Module):
-
-    def __init__(self, h, w, outputs):
-        super(DQN, self).__init__()
-        self.n_actions = outputs
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
-        self.bn1 = nn.BatchNorm2d(16)
-        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
-        self.bn2 = nn.BatchNorm2d(32)
-        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
-        self.bn3 = nn.BatchNorm2d(32)
-
-        # Number of Linear input connections depends on output of conv2d layers
-        # and therefore the input image size, so compute it.
-        def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
-        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
-        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
-        self.linear_input_size = convw * convh * 32
-        self.head = nn.Linear(self.linear_input_size, outputs)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = F.relu(self.bn1(self.conv1(x)))
-        x = F.relu(self.bn2(self.conv2(x)))
-        x = F.relu(self.bn3(self.conv3(x)))
-        self.z = x
-        return self.head(x.view(x.size(0), -1))
-
-class Attacker(nn.Module):
-    def __init__(self, DQN, norm_bound):
-        self.noise = nn.Linear(DQN.linear_input_size, DQN.linear_input_size)
-        self.norm_bound = norm_bound
-
-    def forward(self, DQN):
-        noise = F.tanh(self.noise(DQN.z)) * self.norm_bound / DQN.linear_input_size
-        x = DQN.z + noise
-        return DQN.head(x.view(x.size(0), -1))
-
-
-def optimize(memory, policy_net, target_net, optimizer, loss_func):
-    if len(memory) < BATCH_SIZE:
+class DQN_Agent:
+    
+    def __init__(self, seed, layer_sizes, lr, sync_freq, exp_replay_size, gamma):
+        torch.manual_seed(seed)
+        self.q_net = self.build_nn(layer_sizes)
+        self.target_net = copy.deepcopy(self.q_net)
+        self.loss_fn = torch.nn.MSELoss()
+        self.optimizer = torch.optim.Adam(self.q_net.parameters(), lr=lr)
+        
+        self.network_sync_freq = sync_freq
+        self.network_sync_counter = 0
+        self.gamma = torch.tensor(gamma).float()
+        self.experience_replay = deque(maxlen = exp_replay_size)  
         return
-
-    transitions = memory.sample(BATCH_SIZE)
-    batch = Transition(*zip(*transitions))
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)))
-    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
-
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
-    next_state_values = torch.zeros(BATCH_SIZE)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    loss = loss_func(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    optimizer.zero_grad()
-    loss.backward()
-    for param in policy_net.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
-
-def optimize_attacker(memory, target_net, attacker, optimizer, loss_func):
-    if len(memory) < BATCH_SIZE:
+        
+    def build_nn(self, layer_sizes):
+        assert len(layer_sizes) > 1
+        layers = []
+        for index in range(len(layer_sizes)-1):
+            linear = nn.Linear(layer_sizes[index], layer_sizes[index+1])
+            act =    nn.Tanh() if index < len(layer_sizes)-2 else nn.Identity()
+            layers += (linear,act)
+        return nn.Sequential(*layers)
+    
+    def get_action(self, state, action_space_len, epsilon, attacker=None):
+        # We do not require gradient at this point, because this function will be used either
+        # during experience collection or during inference
+        if attacker is None:
+            with torch.no_grad():
+                Qp = self.q_net(torch.from_numpy(state).float())
+            _, A = torch.max(Qp, axis=0)
+            A = A if torch.rand(1,).item() > epsilon else torch.randint(0,action_space_len,(1,))
+            return A
+        else:
+            pass
+    
+    def get_q_next(self, state):
+        with torch.no_grad():
+            qp = self.target_net(state)
+        q, _ = torch.max(qp, axis=1)    
+        return q
+    
+    def collect_experience(self, experience):
+        self.experience_replay.append(experience)
         return
-
-    transitions = memory.sample(BATCH_SIZE)
-    batch = Transition(*zip(*transitions))
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None, batch.next_state)))
-    non_final_next_states = torch.cat([s for s in batch.next_state if s is not None])
-
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
-
-    state_action_values = target_net(state_batch).gather(1, action_batch)
-    next_state_values = torch.zeros(BATCH_SIZE)
-    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    loss = -loss_func(state_action_values, expected_state_action_values.unsqueeze(1))
-
-    optimizer.zero_grad()
-    loss.backward()
-    for param in attacker.parameters():
-        param.grad.data.clamp_(-1, 1)
-    optimizer.step()
+    
+    def sample_from_experience(self, sample_size):
+        if(len(self.experience_replay) < sample_size):
+            sample_size = len(self.experience_replay)   
+        sample = random.sample(self.experience_replay, sample_size)
+        s = torch.tensor([exp[0] for exp in sample]).float()
+        a = torch.tensor([exp[1] for exp in sample]).float()
+        rn = torch.tensor([exp[2] for exp in sample]).float()
+        sn = torch.tensor([exp[3] for exp in sample]).float()   
+        return s, a, rn, sn
+    
+    def train(self, batch_size ):
+        s, a, rn, sn = self.sample_from_experience( sample_size = batch_size)
+        if(self.network_sync_counter == self.network_sync_freq):
+            self.target_net.load_state_dict(self.q_net.state_dict())
+            self.network_sync_counter = 0
+        
+        # predict expected return of current state using main network
+        qp = self.q_net(s)
+        pred_return, _ = torch.max(qp, axis=1)
+        
+        # get target return using target network
+        q_next = self.get_q_next(sn)
+        target_return = rn + self.gamma * q_next
+        
+        loss = self.loss_fn(pred_return, target_return)
+        self.optimizer.zero_grad()
+        loss.backward(retain_graph=True)
+        self.optimizer.step()
+        
+        self.network_sync_counter += 1       
+        return loss.item()
--- train_DQN.py
+++ train_DQN.py
@@ -1,59 +1,65 @@
 import gym
-from itertools import count
+from network import DQN_Agent
+from vars import *
+import matplotlib.pyplot as plt
+import copy
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-
-from network import DQN, optimize
-from utils import get_screen, ReplayMemory, Action_Selector, plot_durations
-from vars import TARGET_UPDATE, N_EPISODE
-
-env = gym.make('CartPole-v0').unwrapped
-env.reset()
-action_selector = Action_Selector()
-
-init_screen = get_screen(env)
-_, _, screen_height, screen_width = init_screen.shape
-n_actions = env.action_space.n
-
-policy_net = DQN(screen_height, screen_width, n_actions)
-target_net = DQN(screen_height, screen_width, n_actions)
-target_net.load_state_dict(policy_net.state_dict())
-target_net.eval()
-
-optimizer = optim.RMSprop(policy_net.parameters(), lr=1e-3)
-loss_func = nn.SmoothL1Loss()
-memory = ReplayMemory(10000)
-episode_durations = []
-
-for i in range(N_EPISODE):
-    env.reset()
-    state = get_screen(env)
-    for t in count():
-        
-        if state is not None:
-            action = action_selector.select_action(policy_net, state)
-            _, reward, done, _ = env.step(action.item())
-            reward = torch.Tensor([reward])
-            
-            if not done:
-                next_state = get_screen(env)
-            else:
-                next_state = None
-                
-            memory.push(state, action, next_state, reward)
-
-            state = next_state
-
-        optimize(memory, policy_net, target_net, optimizer, loss_func)
-        
-        if done:
-            episode_durations.append(t + 1)
-            plot_durations(episode_durations)
+env = gym.make('CartPole-v0')
+input_dim = env.observation_space.shape[0]
+output_dim = env.action_space.n
+agent = DQN_Agent(seed=1423, layer_sizes=[input_dim, 64, output_dim], lr=LR, sync_freq=SYNC_FREQ, exp_replay_size=EXP_REPLAY_SIZE, gamma=GAMMA)
+   
+index = 0
+for i in range(EXP_REPLAY_SIZE):
+    obs = env.reset()
+    done = False
+    while (not done):
+        A = agent.get_action(obs, env.action_space.n, epsilon=1)
+        obs_next, reward, done, _ = env.step(A.item())
+        agent.collect_experience([obs, A.item(), reward, obs_next])
+        obs = obs_next
+        index += 1
+        if(index > EXP_REPLAY_SIZE):
             break
+            
+durations = []
+index = EXP_REPLAY_SIZE / 2
+epsilon = EPS_START
+best_agent = agent
 
-    if i % TARGET_UPDATE == 0:
-        target_net.load_state_dict(policy_net.state_dict())
-        torch.save(target_net.state_dict(), f'models/target_net.pth')
+for i in range(N_EPISODES):
+    obs, done, duration = env.reset(), False, 0
+    while (not done):
+        duration += 1 
+        A = agent.get_action(obs, env.action_space.n, epsilon)
+        obs_next, reward, done, _ = env.step(A.item())
+        agent.collect_experience([obs, A.item(), reward, obs_next])
+       
+        obs = obs_next
+        index += 1
+        
+        if(index > EXP_REPLAY_SIZE / 2):
+            index = 0
+            for j in range(4):
+                loss = agent.train(batch_size=BATCH_SIZE)
 
+    if epsilon > EPS_END:
+        epsilon -= (1 / EPS_DECAY)
+
+    durations.append(duration)
+    if duration > max(durations):
+        print('hello')
+        best_agent = copy.deepcopy(agent)
+
+plt.plot(durations)
+plt.savefig('hello.jpg')
+plt.close()
+
+env = gym.make('CartPole-v0')
+for i in range(10):
+    obs, done, r = env.reset(), False, 0
+    while (not done):
+        A =  best_agent.get_action(obs, env.action_space.n, epsilon = 0)
+        obs, reward, done, _ = env.step(A.item())
+        r += 1
+    print(r)
